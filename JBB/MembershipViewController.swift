cf9934e9068f5443400b93e0733953985c13e846
Robert Keffury <bobby.keffury@me.com>
2020-05-01 16:59:32 -0700

    Added membership storyboard and file

---
+++ JBB/MembershipViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MembershipViewController.swift
+//  JBB
+//
+//  Created by Bobby Keffury on 5/1/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class MembershipViewController: UIViewController {
+
+    //MARK: - Properties
+    
+    //MARK: - Outlets
+    
+    //MARK: - Views
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    //MARK: - Methods
+    
+    //MARK: - Actions
+    
+    //MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+}
