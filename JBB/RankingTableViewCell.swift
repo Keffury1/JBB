6d0de914b84f6dfb28a356e5a7d791187ffb92ab
Robert Keffury <bobby.keffury@me.com>
2020-05-01 16:22:14 -0700

    Added ranking storyboard, file & wired them up

---
+++ JBB/RankingTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  RankingTableViewCell.swift
+//  JBB
+//
+//  Created by Bobby Keffury on 5/1/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class RankingTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var rankingTitleLabel: UILabel!
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var thirdLabel: UILabel!
+    
+}
---
+++ JBB/RankingsTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  RankingsTableViewController.swift
+//  JBB
+//
+//  Created by Bobby Keffury on 5/1/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class RankingsTableViewController: UITableViewController {
+    
+    //MARK: - Properties
+    
+    //MARK: - Outlets
+    
+    //MARK: - Views
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "rankingCell", for: indexPath) as? RankingTableViewCell else { return UITableViewCell() }
+        
+        return cell
+    }
+    //MARK: - Methods
+    
+    //MARK: - Actions
+    
+    //MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+}
