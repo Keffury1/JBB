9c06cb25213a02f106dc676a5359b8b62793a45c
Robert Keffury <bobby.keffury@me.com>
2020-05-01 13:50:11 -0700

    Added storyboard and laid out tableView

---
+++ JBB/ArticleTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  ArticleTableViewCell.swift
+//  JBB
+//
+//  Created by Bobby Keffury on 5/1/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var articleImageView: UIImageView!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var exportButton: UIButton!
+    
+    @IBAction func exportButtonTapped(_ sender: Any) {
+        
+    }
+    
+}
