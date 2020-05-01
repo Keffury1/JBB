547bc4e193fa5ea5b3ed4606aafc27eb9400e6ce
Robert Keffury <bobby.keffury@me.com>
2020-05-01 14:48:27 -0700

    Added necessary details to podcast page

---
+++ JBB/PodcastTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastTableViewCell.swift
+//  JBB
+//
+//  Created by Bobby Keffury on 5/1/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class PodcastTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var minutesRemainingLabel: UILabel!
+}
