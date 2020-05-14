99e9e67777de623e511013fa03d60f1cd8587e6f
Robert Keffury <bobby.keffury@me.com>
2020-05-13 18:05:11 -0700

    added collectionView to display memberships.

---
+++ JBB/MembershipCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  MembershipCollectionViewCell.swift
+//  JBB
+//
+//  Created by Bobby Keffury on 5/13/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class MembershipCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var archivedLabel: UILabel!
+    @IBOutlet weak var premiumLabel: UILabel!
+    @IBOutlet weak var adFreeLabel: UILabel!
+    @IBOutlet weak var annualSubscriptionLabel: UILabel!
+
+}
--- JBB/MembershipViewController.swift
+++ JBB/MembershipViewController.swift
@@ -8,14 +8,16 @@
 
 import UIKit
 
-class MembershipViewController: UIViewController {
+class MembershipViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     //MARK: - Properties
+    
+    var memberships: [Membership] = []
     
     //MARK: - Outlets
     
     @IBOutlet weak var subscribeButton: UIButton!
-    @IBOutlet weak var pricingImageView: UIImageView!
+    @IBOutlet weak var membershipCollectionView: UICollectionView!
     
     //MARK: - Views
     
@@ -23,14 +25,51 @@ class MembershipViewController: UIViewController {
         super.viewDidLoad()
         
         setupSubviews()
+        addMembership()
+        membershipCollectionView.delegate = self
+        membershipCollectionView.dataSource = self
     }
     
     
     //MARK: - Methods
     
     private func setupSubviews() {
-        subscribeButton.layer.cornerRadius = 20.0
-        pricingImageView.layer.cornerRadius = 20.0
+        subscribeButton.layer.cornerRadius = 10.0
+        subscribeButton.layer.borderWidth = 2.0
+        subscribeButton.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    func addMembership() {
+        let oneYear = Membership(year: "1 Year", price: "$1.99 / month", archived: "✓ Archived Content", premium: "✓ Premium Content", adFree: "✓ Ad-Free", annualSubscription: nil)
+        let twoYear = Membership(year: "2 Year", price: "$1.67 / month", archived: "✓ Archived Content", premium: "✓ Premium Content", adFree: "✓ Ad-Free", annualSubscription: "✓ Annual Subscription")
+        let fiveYear = Membership(year: "5 Year", price: "$1.25 / month", archived: "✓ Archived Content", premium: "✓ Premium Content", adFree: "✓ Ad-Free", annualSubscription: "✓ Annual Subscription")
+        memberships.append(oneYear)
+        memberships.append(twoYear)
+        memberships.append(fiveYear)
+        
+        membershipCollectionView.reloadData()
+    }
+    
+    //MARK: - Collection View Delegate
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return memberships.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "membershipCell", for: indexPath) as? MembershipCollectionViewCell else { return UICollectionViewCell() }
+        
+        let membership = memberships[indexPath.row]
+        
+        cell.yearLabel.text = membership.year
+        cell.priceLabel.text = membership.price
+        cell.archivedLabel.text = membership.archived
+        cell.premiumLabel.text = membership.premium
+        cell.adFreeLabel.text = membership.adFree
+        cell.annualSubscriptionLabel.text = membership.annualSubscription
+        cell.layer.cornerRadius = 20.0
+        
+        return cell
     }
     
     //MARK: - Actions
