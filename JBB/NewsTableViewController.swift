a26a43a2daf1ae3286b82396fbb84c64e89a1fd3
Robert Keffury <bobby.keffury@me.com>
2020-05-26 14:13:14 -0700

    Setup method to fetch posts and load tableView.

--- JBB/NewsTableViewController.swift
+++ JBB/NewsTableViewController.swift
@@ -12,27 +12,53 @@ class NewsTableViewController: UITableViewController {
 
     //MARK: - Properties
     
+    var postController = PostController()
+    var posts: [Post] = []
+    
     //MARK: - Outlets
     
     //MARK: - Views
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupTableViews()
     }
     
     //MARK: - Table view data source
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return posts.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "articleCell", for: indexPath) as? ArticleTableViewCell else { return UITableViewCell() }
         
+        let post = posts[indexPath.row]
+        
+        cell.postTitleLabel.text = post.title.rendered
+        cell.textView.text = post.content.rendered
+        
         return cell
     }
     
     //MARK: - Methods
+    
+    func setupTableViews() {
+        postController.fetchPosts { (posts, error) in
+            if let error = error {
+                print("Error fetching posts for tableView: \(error)")
+                return
+            }
+            
+            guard let posts = posts else {
+                return
+            }
+            
+            self.posts = posts
+            self.tableView.reloadData()
+        }
+    }
     
     //MARK: - Actions
     
