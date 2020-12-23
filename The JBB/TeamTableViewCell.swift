efe7790144d63ad47cfbf31d539c7dfad11727d8
Robert Keffury <bobby.keffury@me.com>
2020-12-22 18:29:11 -0800

    Added Teams Table View

---
+++ The JBB/TeamTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TeamTableViewCell.swift
+//  The JBB
+//
+//  Created by Bobby Keffury on 12/22/20.
+//
+
+import UIKit
+
+class TeamTableViewCell: UITableViewCell {
+
+    //MARK: - Outlets
+    
+    @IBOutlet weak var teamImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
