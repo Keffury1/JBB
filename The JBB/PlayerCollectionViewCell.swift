02b811c9c92895ec260e7c96cfa7a85ecdaa6ed9
Robert Keffury <bobby.keffury@me.com>
2021-01-02 16:08:09 -0800

    Added Roster Detail

---
+++ The JBB/PlayerCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PlayerCollectionViewCell.swift
+//  The JBB
+//
+//  Created by Bobby Keffury on 1/2/21.
+//
+
+import UIKit
+
+class PlayerCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var positionLabel: UILabel!
+    @IBOutlet weak var batThrowLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var hometownLabel: UILabel!
+}
--- The JBB/TeamDetailViewController.swift
+++ The JBB/TeamDetailViewController.swift
@@ -15,10 +15,16 @@ class TeamDetailViewController: UIViewController {
     
     // MARK: - Outlets
     
+    @IBOutlet weak var playerSearchBar: UISearchBar!
+    @IBOutlet weak var rosterCollectionView: UICollectionView!
+    
     // MARK: - Views
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        playerSearchBar.delegate = self
+        rosterCollectionView.dataSource = self
     }
     
     // MARK: - Methods
@@ -29,4 +35,25 @@ class TeamDetailViewController: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     }
+}
+
+extension TeamDetailViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        guard let team = team else { return 1 }
+        return team.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "playerCell", for: indexPath) as? PlayerCollectionViewCell, let team = team else { return UICollectionViewCell() }
+        
+        let player = team[indexPath.row]
+        
+        
+        
+        return cell
+    }
+}
+
+extension TeamDetailViewController: UISearchBarDelegate {
+    
 }
